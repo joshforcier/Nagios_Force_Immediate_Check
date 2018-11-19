55fffbccf2e9beff51f93a0c9932d4bd94b89341
joshforcier <joshforcier@gmail.com>
2018-11-19 11:25:18 -0600

    Update forceimmediatecheck.inc.php

--- forceimmediatecheck.inc.php
+++ forceimmediatecheck.inc.php
@@ -1,46 +1,47 @@
 <?php
-
-// Force Immediate Check Component
+//
+// Mass Immediate Check Component
+// 
 
 require_once(dirname(__FILE__) . '/../componenthelper.inc.php');
 
-$forceimmediatecheck_component_name = "forceimmediatecheck";
-forceimmediatecheck_component_init();
+$massimmediatecheck_component_name = "massimmediatecheck";
+massimmediatecheck_component_init();
 
 ////////////////////////////////////////////////////////////////////////
 // COMPONENT INIT FUNCTIONS
 ////////////////////////////////////////////////////////////////////////
 
-function forceimmediatecheck_component_init()
+function massimmediatecheck_component_init()
 {
-    global $forceimmediatecheck_component_name;
-    $versionok = forceimmediatecheck_component_checkversion();
-    $desc = _("This component allows administrators to force an immediate check of hosts and services.");
+    global $massimmediatecheck_component_name;
+    $versionok = massimmediatecheck_component_checkversion();
+    $desc = _("This component allows administrators to force an immediate check on hosts and services.");
 
     if (!$versionok) {
         $desc = "<b>" . _("Error: This component requires Nagios XI 2009R1.2B or later.") . "</b>";
     }
 
     $args = array(
-        COMPONENT_NAME => $forceimmediatecheck_component_name,
+        COMPONENT_NAME => $massimmediatecheck_component_name,
         COMPONENT_VERSION => '1.0.0',
         COMPONENT_DATE => '10/31/2018',
         COMPONENT_AUTHOR => "Josh Forcier",
         COMPONENT_DESCRIPTION => $desc,
-        COMPONENT_TITLE => _("Force Immediate Check"),
+        COMPONENT_TITLE => _("Mass Immediate Check"),
         COMPONENT_REQUIRES_VERSION => 500
     );
 
     // Register this component with XI
-    register_component($forceimmediatecheck_component_name, $args);
+    register_component($massimmediatecheck_component_name, $args);
 
     // Register the addmenu function
     if ($versionok) {
-        register_callback(CALLBACK_MENUS_INITIALIZED, 'forceimmediatecheck_component_addmenu');
+        register_callback(CALLBACK_MENUS_INITIALIZED, 'massimmediatecheck_component_addmenu');
     }
 }
 
-function forceimmediatecheck_component_checkversion()
+function massimmediatecheck_component_checkversion()
 {
     if (!function_exists('get_product_release'))
         return false;
@@ -49,14 +50,14 @@ function forceimmediatecheck_component_checkversion()
     return true;
 }
 
-function forceimmediatecheck_component_addmenu($arg = null)
+function massimmediatecheck_component_addmenu($arg = null)
 {
     if (is_readonly_user(0)) {
         return;
     }
 
-    global $forceimmediatecheck_component_name;
-    $urlbase = get_component_url_base($forceimmediatecheck_component_name);
+    global $massimmediatecheck_component_name;
+    $urlbase = get_component_url_base($massimmediatecheck_component_name);
 
     $mi = find_menu_item(MENU_HOME, "menu-home-acknowledgements", "id");
     if ($mi == null) {
@@ -72,8 +73,8 @@ function forceimmediatecheck_component_addmenu($arg = null)
 
     add_menu_item(MENU_HOME, array(
         "type" => "link",
-        "title" => _("Force Immediate Check"),
-        "id" => "menu-home-forceimmediatecheck",
+        "title" => _("Mass Immediate Check"),
+        "id" => "menu-home-massimmediatecheck",
         "order" => $neworder,
         "opts" => array(
             "href" => $urlbase . "/index.php"
